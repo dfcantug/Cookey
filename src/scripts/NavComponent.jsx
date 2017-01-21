58122e17efbf614ccd26833141775804914b0e64
Mikael <mantism@seas.upenn.edu>
2017-01-21 14:00:12 -0500

    used react bootstrap for navbar

--- src/scripts/NavComponent.jsx
+++ src/scripts/NavComponent.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
+import {Navbar, Nav, NavbarHeader, NavbarBrand, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
-class NavBar extends React.Component {
+class NavComp extends React.Component {
   render() {
     return (
-      <div className = "row" id = "nav_bar">
-        <div className = "col-md-1 btn">
-          <a href="/"><img style={{width: 60}, {height: 60}} src={'https://s3.amazonaws.com/cookey/cookey_logo.png'} /> </a>
-        </div>
-        <div className = "col-md-5">Search Bar</div>
-        <div className = "col-md-2 btn btn-secondary" ><a href="/cookbook">Cook Book</a></div>
-        <div className = "col-md-2 btn btn-secondary"><a href="/forum">Forum</a></div>
-        <div className = "col-md-2 btn btn-secondary"><a href="/login">Login/Sign Up</a></div>
-      </div>
+          <Navbar style={{height: 65 + 'px'}}>
+                <Navbar.Header>
+                      <Navbar.Brand>
+                         <a href="/"><img style={{width: 40 + 'px'}, {height: 40 + 'px'}} src={'https://s3.amazonaws.com/cookey/cookey_logo.png'} /> </a>
+                      </Navbar.Brand>
+               </Navbar.Header>
+               <Nav>
+                     <NavItem eventKey={1} href="#">Link</NavItem>
+                     <NavItem eventKey={2} href="#">Link</NavItem>
+                     <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                           <MenuItem eventKey={3.1}>Action</MenuItem>
+                           <MenuItem eventKey={3.2}>Another action</MenuItem>
+                           <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                           <MenuItem divider />
+                           <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                     </NavDropdown>
+               </Nav>
+     </Navbar>
     );
-  }
+  };
 }
 
-export default NavBar;
+export default NavComp;
