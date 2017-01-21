ea5df6e911f38cf509d718c84ce1f38fa9b0fdbc
Mikael <mantism@seas.upenn.edu>
2017-01-21 04:48:21 -0500

    pushing logo

--- src/scripts/NavComponent.jsx
+++ src/scripts/NavComponent.jsx
@@ -7,10 +7,9 @@ class NavBar extends React.Component {
       <div className = "row" id = "nav_bar">
         <div className = "col-md-1 btn btn-secondary">
 
-                     <img style={{width: 35}, {height: 35}} src={'https://s3.amazonaws.com/cookey/cookey_logo.png'} />
+                     <img style={{width: 60}, {height: 60}} src={'https://s3.amazonaws.com/cookey/cookey_logo.png'} />
 
       </div>
-        <div className = "col-lg-1 btn btn-secondary">Logo</div>
         <div className = "col-lg-5">Search Bar</div>
         <div className = "col-lg-2 btn btn-secondary" ><a href="/cookbook">Cook Book</a></div>
         <div className = "col-lg-2 btn btn-secondary">Forum</div>
