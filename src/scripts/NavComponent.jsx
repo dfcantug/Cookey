c619a4f3c74991ee37357a7217044606f6bef436
juezhou <juezhou@seas.upenn.edu>
2017-01-21 05:04:19 -0500

    push to additional margin fix

--- src/scripts/NavComponent.jsx
+++ src/scripts/NavComponent.jsx
@@ -5,11 +5,9 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className = "row" id = "nav_bar">
-        <div className = "col-md-1 btn btn-secondary">
-
-                     <img style={{width: 60}, {height: 60}} src={'https://s3.amazonaws.com/cookey/cookey_logo.png'} />
-
-      </div>
+        <div className = "col-md-1 btn">
+          <img style={{width: 60}, {height: 60}} src={'https://s3.amazonaws.com/cookey/cookey_logo.png'} />
+        </div>
         <div className = "col-lg-5">Search Bar</div>
         <div className = "col-lg-2 btn btn-secondary" ><a href="/cookbook">Cook Book</a></div>
         <div className = "col-lg-2 btn btn-secondary">Forum</div>
