d0195374083a331fc7337ee0188444eee27af8ae
Mikael <mantism@seas.upenn.edu>
2017-01-21 01:16:00 -0500

    sets up express, react, babel, and webpack

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path = require('path');
+ var app = express();
+ var port = process.env.PORT || 3000;
+
+ app.set('port', port);
+ app.use(express.static(__dirname));
+ app.set('views', __dirname + '/src/views');
+ app.set('scripts', __dirname + '/src/scripts');
+ app.engine('html', require('ejs').__express);
+ app.set('view engine', 'html');
+
+ app.use(express.static(path.join(__dirname, '/')));
+ app.get("/", function (req,res) {
+       res.render('index.ejs');
+ });
+ app.listen(port);
+ console.log("listening on port " + port);
