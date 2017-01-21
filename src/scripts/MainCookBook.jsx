4fe0f664522b2c8524af787f1d886307a19fdac3
David Cantu <davigfre@gmail.com>
2017-01-21 13:24:56 -0500

    Sets up basic react components in the cookbooks

--- src/scripts/MainCookBook.jsx
+++ src/scripts/MainCookBook.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import {render} from 'react-dom';
-
+import NavBar from './NavComponent.jsx';
 
 class MainPageCookBook extends React.Component {
   render() {
     return (
-      <div> asdasd</div>
+      <div>
+      <NavBar />
+      <Recipe />
+    </div>
     );
   };
 }
 
+
+class Recipe extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+        title: 'Current Recipe',
+        ingredients: Array(10).fill('ingredient')
+      };
+  }
+
+  render() {
+    var ingredientsDivs = [];
+    for (var i = 0; i < this.state.ingredients.length; i++){
+      ingredientsDivs.push(<span> {this.state.ingredients[i]}</span>);
+    }
+    return(
+      <div>
+    <div> {this.state.title} </div>
+    <div> {ingredientsDivs} </div>
+  </div>
+  )
+  }
+}
+
+class Ingredient extends React.Component{
+
+}
 render(<MainPageCookBook/>, document.getElementById('rootCookBook'));
--- src/scripts/NavComponent.jsx
+++ src/scripts/NavComponent.jsx
@@ -6,7 +6,7 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className = "row" id = "nav_bar">
-        <div className = "col-lg-1 btn btn-secondary">Logo</div>
+        <div className = "col-lg-1 btn btn-secondary"><a href="/">Logo</a></div>
         <div className = "col-lg-5">Search Bar</div>
         <div className = "col-lg-2 btn btn-secondary" ><a href="/cookbook">Cook Book</a></div>
         <div className = "col-lg-2 btn btn-secondary">Forum</div>
