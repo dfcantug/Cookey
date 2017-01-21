8ae0220b36e4932a2be921cd295857a420e73182
Mikael <mantism@seas.upenn.edu>
2017-01-21 04:25:51 -0500

    committing logo

--- src/scripts/NavComponent.jsx
+++ src/scripts/NavComponent.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {render} from 'react-dom';
-
 
 class NavBar extends React.Component {
   render() {
     return (
       <div className = "row" id = "nav_bar">
-        <div className = "col-md-1 btn btn-secondary">Logo</div>
+        <div className = "col-md-1 btn btn-secondary">
+
+                     <img  style={{width: 64 + 'px'},{height: 64 + 'px'}} src={'https://s3.amazonaws.com/cookey/cookey_logo.svg'} />
+
+      </div>
         <div className = "col-md-8">Search Bar</div>
-        <div className = "col-md-1 btn btn-secondary" > Cook Book</div>      
+        <div className = "col-md-1 btn btn-secondary" > Cook Book</div>
         <div className = "col-md-1 btn btn-secondary">Forum</div>
         <div className = "col-md-1 btn btn-secondary">Login/Sign Up</div>
       </div>
